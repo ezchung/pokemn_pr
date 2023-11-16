a02265260c5e750cc101fc2294b8913420311a8b
Ezra Chung <ezra.chung97@gmail.com>
2023-11-16 12:26:17 -0800

    Created Pokemon List type and created component

--- pokedex-pr/src/Pokedex.tsx
+++ pokedex-pr/src/Pokedex.tsx
@@ -1,6 +1,25 @@
 import './Pokedex.css';
 import Pokecard from './Pokecard';
+import {Pokemon} from './Pokecard';
 /**
- * Used to reder a squence of Pokecard components
+ * Used to render a sequence of Pokecard components
  * 
+ *  Input: Array of objects with information about pokemons
+ *  - Object should include
+ *         [{ id, name, type, base_experience }, ......]
+ *
+ * Output: Div with class name of Pokdex
  */
+type PokemonList = Pokemon[];
+
+function Pokedex({pokemons}: {pokemons: PokemonList}){
+    return(
+        <div className="Pokedex container">
+            <div className="row">
+                {pokemons.map(p=> <Pokecard pokemon={p}/>)}
+            </div>
+        </div>
+    )
+}
+
+export default Pokedex;
