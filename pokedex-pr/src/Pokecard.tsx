bfdf933c466c2014cabe4878c3134905fa2682d4
Ezra Chung <ezra.chung97@gmail.com>
2023-11-16 14:18:33 -0800

    Attempt to edit container and allow only 4 cards per row max

--- pokedex-pr/src/Pokecard.tsx
+++ pokedex-pr/src/Pokecard.tsx
@@ -22,12 +22,14 @@ function Pokecard({ pokemon }: { pokemon: Pokemon }){
     const POKE_IMG = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`
 
     return(
-        <div className='Pokecard col-3'>
-            <div className='Pokecard-inner'>
-                <p className="Pokecard-name">{pokemon.name}</p>
-                <img className="Pokecard-img"src={POKE_IMG} alt={pokemon.name} />
-                <p className="Pokecard-type">Type: {pokemon.type}</p>
-                <p className="Pokecard-exp">Exp: {pokemon.base_experience}</p>
+        <div className='Pokecard'>
+            <div className='Pokecard-container'>
+                <div className='Pokecard-inner'>
+                    <p className="Pokecard-name">{pokemon.name}</p>
+                    <img className="Pokecard-img"src={POKE_IMG} alt={pokemon.name} />
+                    <p className="Pokecard-type">Type: {pokemon.type}</p>
+                    <p className="Pokecard-exp">Exp: {pokemon.base_experience}</p>
+                </div>
             </div>
         </div>
     )
