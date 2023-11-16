a3d87eff147cdf1b7b689e27770950d7894641a1
Ezra Chung <ezra.chung97@gmail.com>
2023-11-16 15:33:42 -0800

    Added shuffle function and add experience of each hand

--- pokedex-pr/src/Pokegame.tsx
+++ pokedex-pr/src/Pokegame.tsx
@@ -19,7 +19,29 @@ function Pokegame({pokemons}: {pokemons: PokemonList}){
         return pkmns;
     }
 
-    
+    //Split the two
+    //Get the experiences of each hand and add them
+    const shuffledDeck = shufflePokemons(pokemons);
+    const player1 = shuffledDeck.slice(0,(shuffledDeck.length/2));
+    const player2 = shuffledDeck.slice((shuffledDeck.length/2));
+
+    //reduce(accumulator, currentVal) exp = accumulator, p is currentObject
+    const player1Exp = player1.reduce((exp,p) => {
+        return exp + p.base_experience
+    },0);
+    const player2Exp = player2.reduce((exp,p) => {
+        return exp + p.base_experience
+    },0);
+
+    console.log(`${player1Exp} is player 1. ${player2Exp} for player 2`)
+    let winner;
+    if(player1Exp > player2Exp) winner = 1;
+    else if(player2Exp > player1Exp) winner = 2;
+    else winner = 0;
+
+    return <div>
+        <p>{`${player1Exp} is player 1. ${player2Exp} for player 2`}</p>
+    </div>
 }
 
 export default Pokegame;
